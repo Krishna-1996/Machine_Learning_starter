b595b7189d422c47eebfc23c1b7c2daa5d5b9b66
Krishna <1904krishna@gmail.com>
2024-03-27 08:00:24 +0530

    Calculating Limits

---
+++ 2. calculus with Python/4 Calculating Limits.py
@@ -0,0 +1,28 @@
+'''Calculating Limits: 
+
+'''
+#FOR CONTINIOUS FUNCTION 
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# range for x design a curve
+x = np.linspace(-10, 10, 1000)#curve start, finish, intervals
+# Plot an equation on y 
+y = x**2 + 2*x + 2
+#Plot this x, y curve
+plt.plot(x, y, label='y=x^2 + 2x + 2')
+plt.xlabel('x')#label x
+plt.ylabel('y')#label y
+plt.title('Plot of y = x^2 + 2x + 2')#title of curve
+
+plt.grid(True)# Add grid
+
+#plt.show()#show  curve
+#Zoom untile we get straight line
+plt.xlim(-1.01,-0.99)
+plt.ylim(0.99,1.01)
+plt.show()#show  curve
+print("**********************************")
+#FOR NON-CONTINIOUS FUNCTION 
+#Equation 2: 
