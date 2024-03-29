50ceef62f27e2da7be24ddff5a0b77e3dda592b1
Krishna <1904krishna@gmail.com>
2024-03-29 07:33:03 +0530

    rules

--- 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
+++ 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
@@ -2,5 +2,31 @@
 In derivative if there is any constant denoted as 'c' then it is consider as null 
 d/dx (25) = 0: Here 25 is contant and its value is nul for being in derivative
 '''
-# The Power Rule for Derivatives
+# 1. The Power Rule for Derivatives
+'''
+d/dx (x^n) = nx^n-1
+Eg: d/dx (x^4) = 4x^3
 
+'''
+# 2. The constant Rule for Derivatives
+'''
+d/dx (c y) = c d/dx(y) = c dy/dx
+
+Eg: y = x^4
+dy/dx =  4x^3
+d/dx 2y = 2(dy/dx) = 2(4x^3)= 8x^3
+
+'''
+# 3. The Sum Rule for Derivatives
+'''
+d(y+w)/dx = dy/dx +dw/dx
+Eg: y = x^4, w = x^9
+d(y+w)/dx = dy/dx +dw/dx
+=> d/dx(x^4) + d/dx(x^9)
+=> 4x^3 + 9x^8
+
+
+
+
+
+'''
