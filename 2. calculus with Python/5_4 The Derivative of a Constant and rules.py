555b4d289a12431175dfe51a659608b96ac5cc50
Krishna <1904krishna@gmail.com>
2024-03-29 06:44:43 +0530

    The Power Rule for Derivatives

--- 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
+++ 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
@@ -2,4 +2,5 @@
 In derivative if there is any constant denoted as 'c' then it is consider as null 
 d/dx (25) = 0: Here 25 is contant and its value is nul for being in derivative
 '''
+# The Power Rule for Derivatives
 
