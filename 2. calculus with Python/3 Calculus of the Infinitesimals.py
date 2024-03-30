6a249f6e4ee30539d5e1b35dbc09fa9711e32092
Krishna <1904krishna@gmail.com>
2024-03-30 09:49:45 +0530

    Added More Info

--- 2. calculus with Python/3 Calculus of the Infinitesimals.py
+++ 2. calculus with Python/3 Calculus of the Infinitesimals.py
@@ -11,7 +11,7 @@ x = np.linspace(-10, 10, 1000)#curve start, finish, intervals
 y = x**2 + 2*x + 2
 #Plot this x, y curve
 plt.plot(x, y, label='y=x^2 + 2x + 2')
-plt.xlabel('x')#label x
+plt.xlabel('X')#label x
 plt.ylabel('y')#label y
 plt.title('Plot of y = x^2 + 2x + 2')#title of curve
 
