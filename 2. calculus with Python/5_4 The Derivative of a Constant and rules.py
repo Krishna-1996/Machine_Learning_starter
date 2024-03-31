120adbb5b09f74e1eeb174f3792195564af9cf8e
Krishna <1904krishna@gmail.com>
2024-03-31 18:21:26 +0530

    derivative

--- 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
+++ 2. calculus with Python/5_4 The Derivative of a Constant and rules.py
@@ -1,6 +1,7 @@
 '''
 In derivative if there is any constant denoted as 'c' then it is consider as null 
-d/dx (25) = 0: Here 25 is contant and its value is nul for being in derivative
+d/dx (25) = 0: 
+Here 25 is contant and its value is nul for being in derivative
 '''
 # 1. The Power Rule for Derivatives
 '''
