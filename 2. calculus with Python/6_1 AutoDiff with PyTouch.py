5da2e13907b68fcef42eaa0b2993087e35a0b47c
Krishna <1904krishna@gmail.com>
2024-04-01 07:43:35 +0530

    6_2 AutoDiff with Tensorflow

---
+++ 2. calculus with Python/6_1 AutoDiff with PyTouch.py
@@ -0,0 +1,43 @@
+'''
+6_1 AutoDiff with PyTouch.
+f(x) y = x^2
+dy/dx = 2x = 2(5) = 10
+'''
+import torch
+x = torch.tensor(5.0)
+x.requires_grad_()#contagiously track gradients through forward pass
+y = x**2
+y.backward()#use autodifff(dy/dx)
+print(x.grad)
+
+#Lets plot the slop as well
+import numpy as np
+import matplotlib.pylab as plt
+#define the function f(x) = x^2
+def f(x):
+    return x**2
+#Define its derivative dy/dx = 2x
+def df(x):
+    return 2*x
+#Generate x values
+x_values = np.linspace(-5, 5, 100)
+
+#Calcuate y values for both f(x) and its derivative
+y_values_f = f(x_values)
+y_values_df = df(x_values)
+
+# Plot the function f(x) = x^2
+plt.plot(x_values, y_values_f, label='f(x) = x^2')
+
+# Plot the derivative df/dx = 2x
+plt.plot(x_values, y_values_df, label="f'(x) = 2x")
+
+# Add labels and legend
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Plot of f(x) = x^2 and its derivative')
+plt.legend()
+
+# Show plot
+plt.grid(True)
+plt.show()
--- 2. calculus with Python/6_1 AutoDiff with Tensorflow andPyTouch.py
+++
@@ -1,4 +0,0 @@
-import torch
-x = torch.tensor(5.0)
-x.requires_grad_()#contagiously track gradients through forward pass
-y = x**2
