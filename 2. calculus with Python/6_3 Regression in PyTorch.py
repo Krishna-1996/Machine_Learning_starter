91532654eec1e5ba93c988bf85bdabf2c53746bb
Krishna <1904krishna@gmail.com>
2024-04-02 07:12:58 +0530

    6_3 Regression in PyTorch

--- 2. calculus with Python/6_3 Regression in PyTorch.py
+++ 2. calculus with Python/6_3 Regression in PyTorch.py
@@ -20,10 +20,11 @@ plt.title("Clinical Trial")
 plt.xlabel("Drug dosage")
 plt.ylabel("Froget fulness")
 plt.scatter(x, y)
-plt.show()
+
 # Next create slope (line) parameter m with a random pf 0.9...
 m = torch.tensor([0.9]).requires_grad_()
 b = torch.tensor([0.1]).requires_grad_()
+
 
 def regression_plot(my_x, my_y, my_m, my_b):
     plt.scatter(my_x, my_y)
@@ -32,3 +33,23 @@ def regression_plot(my_x, my_y, my_m, my_b):
 
     plt.set_xlim([x_max,x_max])
     plt.scatter([x_min, x_max], [y_min, y_max])
+    plt.show()
+
+regression_plot(x,y,m,b)
+'''
+def regression_plot(my_x, my_y, my_m, my_b):
+    fig, ax = plt.subplots()
+    ax.scatter(my_x, my_y)
+
+    x_min, x_max = ax.get_xlim()
+    y_min, y_max = my_m * x_min + my_b, my_m * x_max + my_b
+
+    ax.set_xlim([x_min, x_max])
+    _ = ax.plot([x_min, x_max], [y_min, y_max])
+
+    plt.show()
+
+regression_plot(x,y,m,b)
+
+
+'''
