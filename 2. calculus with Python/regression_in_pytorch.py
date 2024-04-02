336ba6f988762d97e715c09f5e86494931af2d94
Krishna <1904krishna@gmail.com>
2024-04-02 08:04:43 +0530

    # Regression in PyTorch

--- 2. calculus with Python/regression_in_pytorch.py
+++ 2. calculus with Python/regression_in_pytorch.py
@@ -52,3 +52,80 @@ def regression_plot(my_x, my_y, my_m, my_b):
 
 regression_plot(x, y, m, b)
 
+'''
+"""**Return to slides here if following *Calculus I* class.**
+
+### Machine Learning
+In four easy steps :)
+
+**Step 1**: Forward pass
+"""
+
+yhat = regression(x, m, b)
+yhat
+
+"""**Step 2**: Compare $\hat{y}$ with true $y$ to calculate cost $C$
+
+There is a PyTorch `MSELoss` method, but let's define it outselves to see how it works. MSE cost is defined by: $$C = \frac{1}{n} \sum_{i=1}^n (\hat{y_i}-y_i)^2 $$
+"""
+
+def mse(my_yhat, my_y):
+    sigma = torch.sum((my_yhat - my_y)**2)
+    return sigma/len(my_y)
+
+C = mse(yhat, y)
+C
+
+"""**Step 3**: Use autodiff to calculate gradient of $C$ w.r.t. parameters"""
+
+C.backward()
+
+m.grad
+
+b.grad
+
+"""**Step 4**: Gradient descent"""
+
+optimizer = torch.optim.SGD([m, b], lr=0.01)
+
+optimizer.step()
+
+"""Confirm parameters have been adjusted sensibly:"""
+
+m
+
+b
+
+regression_plot(x, y, m, b)
+
+"""We can repeat steps 1 and 2 to confirm cost has decreased:"""
+
+C = mse(regression(x, m, b), y)
+C
+
+"""Put the 4 steps in a loop to iteratively minimize cost toward zero:"""
+
+epochs = 1000
+for epoch in range(epochs):
+
+    optimizer.zero_grad() # Reset gradients to zero; else they accumulate
+
+    yhat = regression(x, m, b) # Step 1
+    C = mse(yhat, y) # Step 2
+
+    C.backward() # Step 3
+    optimizer.step() # Step 4
+
+    print('Epoch {}, cost {}, m grad {}, b grad {}'.format(epoch, '%.3g' % C.item(), '%.3g' % m.grad.item(), '%.3g' % b.grad.item()))
+
+regression_plot(x, y, m, b)
+
+m.item()
+
+b.item()
+
+"""**N.B.**: The model doesn't perfectly approximate the slope (-0.5) and $y$-intercept (2.0) used to simulate the outcomes $y$ at the top of this notebook. This reflects the imperfectness of the sample of eight data points due to adding random noise during the simulation step. In the real world, the best solution would be to sample additional data points: The more data we sample, the more accurate our estimates of the true underlying parameters will be."""
+
+
+
+'''
