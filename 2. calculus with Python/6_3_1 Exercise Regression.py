db0ea495db97660177018e46393173b7b7296d70
Krishna <1904krishna@gmail.com>
2024-04-03 09:27:31 +0530

    Exercise 1: Regression in PyTorch

--- 2. calculus with Python/6_3_1 Exercise Regression.py
+++ 2. calculus with Python/6_3_1 Exercise Regression.py
@@ -10,6 +10,26 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Value of  x and y:
-x = 2 
+print("Find the slope of x**2 + 2*x + 2, where x = 2")
+#Step 1: Define equation in function
+def func(x):
+    return x**2 + 2*x + 2 
+
+#Step 2: Define x as PyTorch tensor
+x = torch.tensor(2.0, requires_grad=True)
+
+#Step 3: Compute the func for y
+y = func(x)
+print(y)
+
+#Step 4: Compute the gradient (slope of 'y' w.r.t to 'x')
+y.backward()
+
+#Step 5: Extract the slope
+slope = x.grad.item()
+print("The slope of y = x^2 + 2x + 2, where x = 2 is:", slope)
+
+
+
+
 
