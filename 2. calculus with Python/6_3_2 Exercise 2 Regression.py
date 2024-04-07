6a8f213573255220312bb1dacead67875f38766f
Krishna-1996 <1904krishna@gmail.com>
2024-04-07 11:45:30 +0530

    Exercise 1:

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -2,10 +2,18 @@
 # x^2 + 2x +2 = y, where x = 2.and
 '''
 ''' 
+#Import libraries:
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
 
 #  find slope
 def fun(x):
     return x**2 + 2*x + 2
+x = torch.tensor(2.0, requires_grad=True)
+print(x)
 y = fun(2)
 print(y)
 
