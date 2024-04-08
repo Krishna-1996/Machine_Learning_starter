dbd8e2c9c9e9e5b57f7d0a9dcc7e46fafbafdc62
Krishna-1996 <1904krishna@gmail.com>
2024-04-08 20:31:38 +0530

    Exercise 1:

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -14,8 +14,15 @@ def fun(x):
     return x**2 + 2*x + 2
 x = torch.tensor(2.0, requires_grad=True)
 print(x)
-y = fun(2)
+
+#Step 3: Compute the func for y
+y = fun(x)
 print(y)
+
+#Step 4: Compute the gradient (slope of 'y' w.r.t to 'x')
+y.backward()
+slope = x.grad.item()
+print("The slope of y = x^2 + 2x + 2, where x = 2 is:", slope)
 
 
 
