75ecf0b461bd4f22bff3fb26364df37cae30265a
Krishna-1996 <1904krishna@gmail.com>
2024-04-08 22:02:47 +0530

    Adding and Testing

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -1,23 +1,42 @@
+import random
 import torch
 import matplotlib.pyplot as plt
-import random
+
 
 #Comment:  Adding and Testing
 
 # Generate an array with 50 score = x values
 hours = [random.randint(0, 9) for _ in range(50)]
 
-print(hours)
 x_hours = torch.tensor(hours)
-print("x_hours: ",x_hours)#x values hours by 50 students
-scores = (x_hours**2 + 2*x_hours + 3)
-y_score = torch.tensor(scores) + torch.normal(mean= torch.zeros(50), std = 0.2)
-print("y_score: ",y_score)
+#print("x_hours: ",x_hours)#x values hours by 50 students
+
+#scores = (x_hours**2 + 2*x_hours + 3)
+y_score = torch.tensor(x_hours**2 + 2*x_hours + 3) + torch.normal(mean= torch.zeros(50), std = 0.2)
+#print("y_score: ",y_score)
 
 #Lets plot a graph with x and y including 0.2 std noise.
-fig, ax = plt.subplots()
+
 plt.title("More hours of study, More score you get")
 plt.xlabel("Hours--->")
 plt.ylabel("Score--->")
-_ = ax.scatter(x_hours, y_score)
+plt.scatter(x_hours, y_score)
+
+m = 2
+b = 3
+#initialize the slope:
+def reg(my_x, my_m, my_b):
+    return my_m*my_x + my_b
+
+#plot the slope:
+def reg_plot(my_x, my_y, my_m, my_b):
+    x_min, x_max = my_x.min().item(), my_x.max().item()
+    y_min = reg(x_min, my_m, my_b)
+    y_max = reg(x_max, my_m, my_b)
+    plt.plot([x_min, x_max], [y_min, y_max], color='red')
+
+reg_plot(x_hours, y_score, m, b)
 plt.show()
+print("Basic done here ")
+
+    
--- 2. calculus with Python/test.py
+++ 2. calculus with Python/test.py
@@ -1,4 +1,36 @@
-import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import random
 
-print("Hello World..!!")
-print("Hellow World")
+# Generate an array with 50 score = x values
+hours = [random.randint(0, 9) for _ in range(50)]
+
+x_hours = torch.tensor(hours)
+original_scores = (x_hours**2 + 2*x_hours + 3)
+noisy_scores = original_scores + torch.normal(mean=torch.zeros(50), std=0.2)
+m = 2
+b = 3
+# Plot the scatter plot
+plt.title("More hours of study, More score you get")
+plt.xlabel("Hours--->")
+plt.ylabel("Score--->")
+plt.scatter(x_hours, noisy_scores)
+
+# Define the regression function
+def reg(my_x, my_m, my_b):
+    return my_m * my_x + my_b
+
+# Plot the regression line
+def reg_plot(my_x, my_y, my_m, my_b):
+    x_min, x_max = my_x.min().item(), my_x.max().item()
+    y_min = reg(x_min, my_m, my_b)
+    y_max = reg(x_max, my_m, my_b)
+    plt.plot([x_min, x_max], [y_min, y_max], color='red')
+
+# Call the reg_plot function to plot the regression line
+reg_plot(x_hours, noisy_scores, m, b)
+
+# Show the plot
+plt.show()
+
+print("Basic done here ")
