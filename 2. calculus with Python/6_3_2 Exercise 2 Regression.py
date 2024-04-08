4b4953e4fd4cf799745f6e4f5d5a1c5d36dfe287
Krishna-1996 <1904krishna@gmail.com>
2024-04-08 20:52:55 +0530

    adding

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -1,50 +1,25 @@
-# Exercise 1: 
-# x^2 + 2x +2 = y, where x = 2.and
-'''
-''' 
-#Import libraries:
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import numpy as np
 import matplotlib.pyplot as plt
 
-#  find slope
-def fun(x):
-    return x**2 + 2*x + 2
-x = torch.tensor(2.0, requires_grad=True)
-print(x)
+x = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7.]) # E.g.: Dosage of drug for treating Alzheimer's disease
+print("x: =",x)
 
-#Step 3: Compute the func for y
-y = fun(x)
-print(y)
+"""The $y$ values were created using the equation of a line $y = mx + b$. This way, we know what the model parameters to be learned are, say, $m = -0.5$ and $b = 2$. Random, normally-distributed noise has been added to simulate sampling error:"""
 
-#Step 4: Compute the gradient (slope of 'y' w.r.t to 'x')
-y.backward()
-slope = x.grad.item()
-print("The slope of y = x^2 + 2x + 2, where x = 2 is:", slope)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#y = -0.5*x + 2 + torch.normal(mean=torch.zeros(8), std=0.2)
+#y1 = 2**x + 5*x + 7 + torch.normal(mean=torch.zeros(8), std=0.2)
+y2 = 2**x + 5*x + 7 + torch.normal(mean=torch.zeros(8), std = 0.4)
+y3 = 2**x + 5*x + 7 + torch.normal(mean=torch.absolute(x), std = 0.4)
+print("x: ",x)
+print("y: ",y)
+print("y1: ",y1)
+print("y2: ",y2)
+print("y3: ",y3)
+fig, ax = plt.subplots()
+plt.title("Clinical Trial")
+plt.xlabel("Drug dosage (mL)")
+plt.ylabel("Forgetfulness")
+_ = ax.scatter(x, y1, y2, y3)
+plt.show()
 
 
