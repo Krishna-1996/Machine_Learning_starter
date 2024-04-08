054713db250f17673a79ee8c645be90ac622be0c
Krishna-1996 <1904krishna@gmail.com>
2024-04-08 21:25:57 +0530

    adding and testing

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -1,3 +1,13 @@
 import torch
 import matplotlib.pyplot as plt
 
+import random
+#Comment:  adding and testing
+# Generate an array with 50 score = x values
+hours = [random.randint(0, 9) for _ in range(50)]
+
+print(hours)
+x_hours = torch.tensor(hours)
+print(x_hours)#x values hours by 50 students
+scores = (x_hours**2 + 2*x_hours + 3)
+y_score = torch.tensor(scores)
