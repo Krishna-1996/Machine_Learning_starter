0a22f9ae4403be610a7cc9b50f1471698daded79
Krishna-1996 <1904krishna@gmail.com>
2024-04-08 21:36:46 +0530

    Adding and Testing

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -13,3 +13,10 @@ print("x_hours: ",x_hours)#x values hours by 50 students
 scores = (x_hours**2 + 2*x_hours + 3)
 y_score = torch.tensor(scores) + torch.normal(mean= torch.zeros(50), std = 0.2)
 print("y_score: ",y_score)
+
+#Lets plot a graph with x and y including 0.2 std noise.
+fig, ax = plt.subplot()
+plt.title("More hours of study, More score you get")
+plt.xlabel("Hours--->")
+plt.ylabel("Score--->")
+_ = ax.scatter(x_hours, y_score)
