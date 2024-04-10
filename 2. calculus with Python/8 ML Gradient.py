4c6e57577cbaad746a4022a09479d8329a849238
Krishna-1996 <1904krishna@gmail.com>
2024-04-10 21:07:29 +0530

    Partial Derivative of Multi variant Function

--- 2. calculus with Python/8 ML Gradient.py
+++ 2. calculus with Python/8 ML Gradient.py
@@ -8,6 +8,23 @@
 --> Back-Propagation
 '''
 # 1. Partial Derivative of Multi variant Function
+#Single Regression
+'''
+z = (x^2 - y^2)
+Let's y = zero(constant)
+Then, 
+==> z = x^2 - 0 #partial derivation of 'z' w.r.t to 'x'.
+==> dz/dx = 2x
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import math
+print("Equation ==> z = (x^2 - y^2)")
+xs = np.linespace(-3, 3, 1000)
+
+
+
 
 
 
