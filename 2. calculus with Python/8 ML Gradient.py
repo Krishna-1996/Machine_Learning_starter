ad259813d9a1cf563c2a017279ee4fdf5b64c43a
Krishna-1996 <1904krishna@gmail.com>
2024-04-10 21:10:17 +0530

    define function for the Equation

--- 2. calculus with Python/8 ML Gradient.py
+++ 2. calculus with Python/8 ML Gradient.py
@@ -19,9 +19,15 @@ Then,
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import math
+import math # for constant pi.
+
+
 print("Equation ==> z = (x^2 - y^2)")
-xs = np.linespace(-3, 3, 1000)
+#define function for the Equation
+def func(my_x, my_y):#z = x**2 -y**2
+    return my_x**2 - my_y**2 
+#define all the x values 1000 from -3 to +3.
+xs = np.linspace(-3, 3, 1000)
 
 
 
