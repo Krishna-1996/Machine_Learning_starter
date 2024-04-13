d85a6a2511e153aa885dde060d192ed459ae4296
Krishna-1996 <1904krishna@gmail.com>
2024-04-13 23:25:33 +0530

    Comment:  Adding and Testing

--- 2. calculus with Python/6_3_2 Exercise 2 Regression.py
+++ 2. calculus with Python/6_3_2 Exercise 2 Regression.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 hours = [random.randint(0, 9) for _ in range(50)]
 
 x_hours = torch.tensor(hours)
-#print("x_hours: ",x_hours)#x values hours by 50 students
+# print("x_hours: ",x_hours)#x values hours by 50 students
 
 #scores = (x_hours**2 + 2*x_hours + 3)
 y_score = torch.tensor(x_hours**2 + 2*x_hours + 3) + torch.normal(mean= torch.zeros(50), std = 0.2)
