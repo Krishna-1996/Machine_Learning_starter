6c1bf079f024e0254228ff451fd4add0251ee8ca
Krishna-1996 <1904krishna@gmail.com>
2024-04-17 22:17:56 +0530

    8_3 Advance Partial Derivatives

--- 2. calculus with Python/8_3 Advance Partial Derivatives.py
+++ 2. calculus with Python/8_3 Advance Partial Derivatives.py
@@ -1,7 +1,7 @@
 # 8_3 Advance Partial Derivatives
 import numpy as np
-import matplotlib.pyplot as plt
-import torch
+#import matplotlib.pyplot as plt
+#import torch
 import math # for constant pi.
 
 '''
