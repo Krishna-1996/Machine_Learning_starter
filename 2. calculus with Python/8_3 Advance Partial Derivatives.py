c088b416bf6040cccf7a2747ad109b3a5c5bacf8
Krishna-1996 <1904krishna@gmail.com>
2024-04-17 21:46:26 +0530

    8_3 Advance Partial Derivatives

--- 2. calculus with Python/8_3 Advance Partial Derivatives.py
+++ 2. calculus with Python/8_3 Advance Partial Derivatives.py
@@ -10,3 +10,22 @@ We need to find the change in vol w.r.t change in l
 It means we need to know partial derivative of dv/dl
 
 '''
+def vol(my_r, my_l):
+    return math.pi * r**2 * l
+
+#Let suppose r = 3 and l = 5
+r = torch.tensor(3.0).requires_grad_()
+l = torch.tensor(5.0).requires_grad_()
+v = vol(r,l)
+print("Volume of Cylinder is: ", v +"m cubic")
+
+
+
+
+
+
+
+
+
+
+
