3d966f3f94181987c043e5bc6b0bd1aae3d6f91d
Krishna-1996 <1904krishna@gmail.com>
2024-04-17 22:10:23 +0530

    8_3 Advance Partial Derivatives

--- 2. calculus with Python/8_3 Advance Partial Derivatives.py
+++ 2. calculus with Python/8_3 Advance Partial Derivatives.py
@@ -11,16 +11,31 @@ It means we need to know partial derivative of dv/dl
 
 '''
 def vol(my_r, my_l):
-    return math.pi * r**2 * l
+    return math.pi * my_r**2 * my_l
 
 #Let suppose r = 3 and l = 5
 r = torch.tensor(3.0).requires_grad_()
 l = torch.tensor(5.0).requires_grad_()
 v = vol(r,l)
-print("Volume of Cylinder is: "+ v +"m cubic")
+print("Volume of Cylinder is: ", v)
+v.backward
+l.grad
+print(l)
+print(math.pi * 3**2)
+v1 = vol(3,6)
+v2 = vol(3,7)
+v_diff = v2-v1
+print(v1, v2)
+print(v_diff)
 
+r.grad
+'''
+dv/dr = 2 * pi *l
+'''
 
-
+delta = 1e-6
+print((vol(3+delta,5)-vol(3,5))/delta)
+#divi
 
 
 
