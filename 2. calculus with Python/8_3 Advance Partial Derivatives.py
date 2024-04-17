28f459017ccf1a772e54742d951982f28fcf32ba
Krishna-1996 <1904krishna@gmail.com>
2024-04-17 21:42:53 +0530

    Advance Partial Derivatives

--- 2. calculus with Python/8_3 Advance Partial Derivatives.py
+++ 2. calculus with Python/8_3 Advance Partial Derivatives.py
@@ -3,3 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import math # for constant pi.
+
+'''
+Volume of Cylinder = pi * r^2 * l
+We need to find the change in vol w.r.t change in l 
+It means we need to know partial derivative of dv/dl
+
+'''
