dc3dbe58f9acee721fee5d3e7e5ce96b5b0fe40a
Krishna-1996 <1904krishna@gmail.com>
2024-04-18 08:48:21 +0530

    dz/dy = 3y^2 + 5

--- 2. calculus with Python/8_3_1 Advance Partial Derivatives_Exercise.py
+++ 2. calculus with Python/8_3_1 Advance Partial Derivatives_Exercise.py
@@ -4,7 +4,11 @@ Partial DErivative for :
 z = y^3 + 5xy
 '''
 import torch 
-'''
-dz/dy = 3y^2 + 5
 
-'''
+# dz/dy = 3y^2 + 5 (Change in z w.r.t to change in y value)
+def z_wrt_y(my_x, my_y):
+    return 3 * my_y**2 + 5 # x is constant here 0
+
+
+
+    
