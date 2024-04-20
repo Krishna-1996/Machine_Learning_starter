4653de4d8925458002e6dacf1cd62e71ad7f1941
Krishna-1996 <1904krishna@gmail.com>
2024-04-20 20:35:53 +0530

    Add minor details

--- 2. calculus with Python/8_4 Recap Linear Regression.py
+++ 2. calculus with Python/8_4 Recap Linear Regression.py
@@ -6,7 +6,7 @@ import torch
 xs = torch.tensor([1,2,3,4,5,6,7,8,9,10])
 ys = torch.tensor([1.344, 2.434, 1.308, 0.554,-1.346, 2.656, 0.244, 0.678, -1.366, -0.244])
 
-#Get slope of line as : y = mx + b
+# Get slope of line as : y = mx + b
 def reg(my_x, my_m, my_b):
     return (my_x*my_m )+ my_b
 
