91adcc998eb64f591d86b5b039ae6792eac58fc0
Krishna-1996 <1904krishna@gmail.com>
2024-04-21 21:36:39 +0530

    added some data

--- 2. calculus with Python/8_4 Recap Linear Regression.py
+++ 2. calculus with Python/8_4 Recap Linear Regression.py
@@ -6,12 +6,12 @@ import torch
 xs = torch.tensor([1,2,3,4,5,6,7,8,9,10])
 ys = torch.tensor([1.344, 2.434, 1.308, 0.554,-1.346, 2.656, 0.244, 0.678, -1.366, -0.244])
 
-# Step 2
+# Step 2:
 # Get slope of line as : y = mx + b
 def reg(my_x, my_m, my_b):
     return (my_x*my_m )+ my_b
 
-# Step 3
+# Step 3:
 # Provide m and b value random as 0.9, 0.1 respectively 
 m = torch.tensor([0.9]).requires_grad_()
 b = torch.tensor([0.1]).requires_grad_()
