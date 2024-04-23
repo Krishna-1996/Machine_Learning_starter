cecc32bb16434d95a5baa05e375cd3483f24ce30
Krishna-1996 <1904krishna@gmail.com>
2024-04-23 06:55:42 +0530

    The Gradient of Quadratic Cost:

--- 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
+++ 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
@@ -3,7 +3,7 @@
 import math 
 print("Hello World...Better Luck...!!")
 def cal(x, y, z, a, ):
-    return ((((x + y)**2 - (z-a)**3) +  ((z-a)**3 - (x + y)**2)) - (x-y+z))/(z-x-y +a)
+    return ((((x + y)**2 - (z-a)**3) +  ((z-a)**3 - (x + y)**2)) - (x-y+z))/(z-x+y +a)
 
 result = cal(2,3,4,7)
 print(result)
