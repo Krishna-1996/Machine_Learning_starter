2292e2f0dcf52f5a78f5433665872190daadc64e
Krishna-1996 <1904krishna@gmail.com>
2024-04-24 07:07:15 +0530

    adding more code

--- 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
+++ 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
@@ -9,10 +9,12 @@ ys = torch.tensor([-1.9875, -1.7832, -0.0041, 0.00, 0.0100, 0.0341, 1.3453, 2.43
 
 #Define regression expression: y = mx + b
 def reg(my_x, my_m, my_b):
-    return my_x*my_x + my_b
+    return my_x*my_m + my_b
 
 #Define m and b values manually 
 m = torch.tensor([0.9]).requires_grad_()
 b = torch.tensor([0.1]).requires_grad_()
 
-#step 1: Forward Pass
+#Step 1: Forward Pass
+yhat = reg(xs, m, b)
+print(yhat)
