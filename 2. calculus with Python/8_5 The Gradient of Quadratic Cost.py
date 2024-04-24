ccb4d4377c437abc9ca9c88b044b050fa3be8df7
Krishna-1996 <1904krishna@gmail.com>
2024-04-24 06:54:39 +0530

    8_5 : The Gradient of Cost on Batch of Data

--- 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
+++ 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
@@ -1,9 +1,1 @@
-# 8_5 : The Gradient of Quadratic Cost: 
-
-import math 
-print("Hello World...Better Luck...!!")
-def cal(x, y, z, a, ):
-    return ((((x + y)**2 - (z-a)**3) +  ((z-a)**3 - (x + y)**2)) + (x-y+z))/((z*x)+y +a)
-
-result = cal(2,3,4,12)
-print(result)
+# 8_5 : The Gradient of Cost on Batch of Data 
