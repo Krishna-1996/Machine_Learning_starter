bf0d77e59e12ebe3913cb0b832ea7138541d7bef
Krishna-1996 <1904krishna@gmail.com>
2024-04-24 07:03:08 +0530

    adding more code

--- 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
+++ 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 
 xs = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])
 ys = torch.tensor([-1.9875, -1.7832, -0.0041, 0.00, 0.0100, 0.0341, 1.3453, 2.4353])
+
+def reg(my_x, my_m, my_b):
+    return my_x*my_x + my_b
+
+m = 
