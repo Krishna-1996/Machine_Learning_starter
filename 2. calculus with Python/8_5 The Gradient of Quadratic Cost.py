3a52985e15d88674290ff559fc13daddb544d272
Krishna-1996 <1904krishna@gmail.com>
2024-04-25 09:33:55 +0530

    some correction done

--- 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
+++ 2. calculus with Python/8_5 The Gradient of Quadratic Cost.py
@@ -19,7 +19,7 @@ b = torch.tensor([0.1]).requires_grad_()
 yhat = reg(xs, m, b)
 print("yhat: ", yhat)
 
-#Step 2: Compare yhat and actual y values and calculate cost "c".
+# Step 2: Compare yhat and actual y values and calculate cost "c".
 #We use Mean square error to do this.
 
 def mse(my_yhat, my_y):
@@ -35,8 +35,7 @@ print(c)
 m.grad
 b.grad
 
-# Step 4: 
-# partial derivative of cost 'c' w.r.t 'm'.
+# Step 4: Partial derivative of cost 'c' w.r.t 'm'.
 pd_m = 2*1/len(ys) * torch.sum((yhat-ys)*xs)
 pd_b = 2*1/len(ys) * torch.sum((yhat-ys))
 
