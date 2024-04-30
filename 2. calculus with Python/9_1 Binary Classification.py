4f1a6ca94afbd69f5b28363e78ef63d81bfc872b
Krishna-1996 <1904krishna@gmail.com>
2024-04-30 07:08:06 +0530

    Integration using Python

--- 2. calculus with Python/9_1 Binary Classification.py
+++ 2. calculus with Python/9_1 Binary Classification.py
@@ -1,3 +1,7 @@
-# Integration using Python
+# Integration using Python Scipy method
 # 
 from scipy.integrate import quad #"Quadrature = numerical integral"
+
+def g(x):
+    return x/2
+quad(g,1,2)
