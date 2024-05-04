88139495ea4201b23abe95dc436037f51c495c89
Krishna-1996 <1904krishna@gmail.com>
2024-05-05 00:03:25 +0530

    Single Variant Linear Regression Model:

--- 4. Machine Learning Foundation/3.2 Supervised_Learning.py
+++ 4. Machine Learning Foundation/3.2 Supervised_Learning.py
@@ -3,4 +3,6 @@
 import pandas as pd
 df = pd.read_csv("E:/Machine Learning/Machine Learning/house_prices.csv")
 # print(df)
-# convert the dataset into binary 0 or 1 for better ynderstanding 
+
+# convert the dataset into binary 0 or 1 for better understanding 
+
