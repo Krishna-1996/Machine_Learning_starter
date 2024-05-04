a35aa604443d89a8f96f2691808bc2373af89509
Krishna-1996 <1904krishna@gmail.com>
2024-05-04 07:02:41 +0530

    added

--- 4. Machine Learning Foundation/1 Data_Preparation The_Foundation_of_ML.py
+++ 4. Machine Learning Foundation/1 Data_Preparation The_Foundation_of_ML.py
@@ -5,5 +5,5 @@ import pandas as pd
 
 #Import csv data file
 data = pd.read_csv("E:/Machine Learning/Machine Learning/AB_NYC_2019.csv")
-data.head
-print(data.info())
+data.head()
+print(data.info())# clmn details 
