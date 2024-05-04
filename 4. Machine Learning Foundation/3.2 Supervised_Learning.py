6ee8f7f8012bb73c9c15762d5da2002321973ee7
Krishna-1996 <1904krishna@gmail.com>
2024-05-05 00:41:19 +0530

    Done with Single Variant Linear Regression Model

--- 4. Machine Learning Foundation/3.2 Supervised_Learning.py
+++ 4. Machine Learning Foundation/3.2 Supervised_Learning.py
@@ -5,7 +5,7 @@ df = pd.read_csv("E:/Machine Learning/Machine Learning/house_prices.csv")
 # print(df) # print the dataset
 
 column_names = list(df.columns.values)
-#print(column_names)# print all columns name
+print(column_names)# print all columns name
 
 # convert the dataset into binary 0 or 1 for better understanding 
 df = pd.get_dummies(df, columns=['mainroad'])
@@ -17,9 +17,31 @@ df = pd.get_dummies(df, columns=['prefarea'])
 df = pd.get_dummies(df, columns=['furnishingstatus'])
 
 # lets start with Linear Regression
-import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+'''
+we need 2 parameters for single LR, as 'x' and 'y'. So here we use, area and price with relation of:
+How much price 'x' would be if the area is 'y'....?
+'''
+#extract area as sq_foot and price as prices from dataset
+area = df['area'].values
+price = df['price'].values
+
+#reshare the data for 1 Dimension 
+x = area.reshape(-1,1) # type: ignore
+
+#Create and train the LR model
+model = LinearRegression()
+model.fit(x, price) # type: ignore
+
+#predict the price as according to the given area.Eg: 1900
+new_area = np.array([[1900]])
+predicted_price = model.predict(new_area)
+print(predicted_price)
+
+
+
 
 
 
