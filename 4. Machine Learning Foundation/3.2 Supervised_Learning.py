fd957c2fde8a4d6dc5db7efca886661b3b3693e9
Krishna-1996 <1904krishna@gmail.com>
2024-05-05 00:17:40 +0530

    added

--- 4. Machine Learning Foundation/3.2 Supervised_Learning.py
+++ 4. Machine Learning Foundation/3.2 Supervised_Learning.py
@@ -6,6 +6,7 @@ df = pd.read_csv("E:/Machine Learning/Machine Learning/house_prices.csv")
 
 column_names = list(df.columns.values)
 #print(column_names)# print all columns name
+
 # convert the dataset into binary 0 or 1 for better understanding 
 df = pd.get_dummies(df, columns=['mainroad'])
 df = pd.get_dummies(df, columns=['guestroom'])
@@ -14,3 +15,11 @@ df = pd.get_dummies(df, columns=['hotwaterheating'])
 df = pd.get_dummies(df, columns=['airconditioning'])
 df = pd.get_dummies(df, columns=['prefarea'])
 df = pd.get_dummies(df, columns=['furnishingstatus'])
+
+# lets start with Linear Regression
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+
