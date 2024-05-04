06ea275891b721009a57e933969e13b6c8f41303
Krishna-1996 <1904krishna@gmail.com>
2024-05-05 00:05:40 +0530

    added

--- 4. Machine Learning Foundation/3.2 Supervised_Learning.py
+++ 4. Machine Learning Foundation/3.2 Supervised_Learning.py
@@ -3,6 +3,7 @@
 import pandas as pd
 df = pd.read_csv("E:/Machine Learning/Machine Learning/house_prices.csv")
 # print(df)
-
+column_names = list(df.columns.values)
+print(column_names)
 # convert the dataset into binary 0 or 1 for better understanding 
-
+#df = pd.get_dummies(df, columns=['mainroad'])
