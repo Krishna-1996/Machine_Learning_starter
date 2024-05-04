989523d621209cad29da1568739debbeafbe4680
Krishna-1996 <1904krishna@gmail.com>
2024-05-05 00:12:22 +0530

    added

--- 4. Machine Learning Foundation/3.2 Supervised_Learning.py
+++ 4. Machine Learning Foundation/3.2 Supervised_Learning.py
@@ -2,13 +2,15 @@
 # Single Variant Linear Regression Model:
 import pandas as pd
 df = pd.read_csv("E:/Machine Learning/Machine Learning/house_prices.csv")
-# print(df)
+# print(df) # print the dataset
+
 column_names = list(df.columns.values)
-print(column_names)
+#print(column_names)# print all columns name
 # convert the dataset into binary 0 or 1 for better understanding 
 df = pd.get_dummies(df, columns=['mainroad'])
-df = pd.get_dummies(df, columns=['mainroad'])
-df = pd.get_dummies(df, columns=['mainroad'])
-df = pd.get_dummies(df, columns=['mainroad'])
-df = pd.get_dummies(df, columns=['mainroad'])
-df = pd.get_dummies(df, columns=['mainroad'])
+df = pd.get_dummies(df, columns=['guestroom'])
+df = pd.get_dummies(df, columns=['basement'])
+df = pd.get_dummies(df, columns=['hotwaterheating'])
+df = pd.get_dummies(df, columns=['airconditioning'])
+df = pd.get_dummies(df, columns=['prefarea'])
+df = pd.get_dummies(df, columns=['furnishingstatus'])
