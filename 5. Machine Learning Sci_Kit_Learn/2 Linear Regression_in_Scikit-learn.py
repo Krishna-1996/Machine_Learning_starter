cf0256afc55ad4a5ee9633423acaa055683685e1
Krishna-1996 <1904krishna@gmail.com>
2024-05-12 13:06:51 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
@@ -10,4 +10,7 @@ iris = load_iris()
 df  = pd.DataFrame(data = iris.data, columns = iris.feature_names)
 print(df.head())#print the whole dataset
 df.shape
+df.plot.scatter(x= 'petal width (cm)', y = 'petal length (cm)', title = 'ScatterPlot')
+print(df.corr())
+print(df.describe())
 
