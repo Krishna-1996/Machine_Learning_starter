e1df2c7ef1b2ca0f809604f49391216f02758c58
Krishna-1996 <1904krishna@gmail.com>
2024-05-12 11:59:23 +0530

    2. Linear Regression in Scikit-learn

--- 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
@@ -3,6 +3,4 @@
 # 2. Linear Regression in Scikit-learn
 # 2. Linear Regression in Scikit-learn
 # 2. Linear Regression in Scikit-learn
-# 2. Linear Regression in Scikit-learn
-# 2. Linear Regression in Scikit-learn
-# 2. Linear Regression in Scikit-learn
+
