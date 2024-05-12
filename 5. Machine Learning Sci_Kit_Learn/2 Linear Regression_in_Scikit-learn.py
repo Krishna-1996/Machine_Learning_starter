4a4720a6a63ca48d7060b3a6e4e3a1f949763da5
Krishna-1996 <1904krishna@gmail.com>
2024-05-12 13:29:59 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
@@ -25,6 +25,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=0.2)
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X_train, Y_train)
+print("intercept: ",model.intercept_)
+print("coefficient: ",model.coef_)
+
+
+# Model Prediction
 
 
 
