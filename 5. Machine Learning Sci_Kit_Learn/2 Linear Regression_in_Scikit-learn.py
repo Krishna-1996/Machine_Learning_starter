7467c8ebba2e779e4ea30688ac42ad6db4bfbb59
Krishna-1996 <1904krishna@gmail.com>
2024-05-12 13:27:21 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
@@ -21,6 +21,10 @@ x = df['petal width (cm)'].values.reshape(-1,1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=0.2)
 
+# Training the model
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, Y_train)
 
 
 
