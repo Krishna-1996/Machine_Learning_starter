f35e601d4fd751e717b2149dc742cb8c22e7fcb7
Krishna-1996 <1904krishna@gmail.com>
2024-05-29 06:17:09 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/2 Linear Regression_in_Scikit-learn.py
@@ -1,8 +1,7 @@
 # 2. Linear Regression in Scikit-learn
 # 2. Linear Regression in Scikit-learn
 # 2. Linear Regression in Scikit-learn
-# 2. Linear Regression in Scikit-learn
-# 
+ 
 
 import pandas as pd
 from sklearn.datasets import load_iris
