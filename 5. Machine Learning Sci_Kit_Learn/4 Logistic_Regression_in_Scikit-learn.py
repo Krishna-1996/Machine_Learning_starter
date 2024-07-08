6b80132920383b3f7f8f24b5082f0bfec17f2adf
Krishna-1996 <1904krishna@gmail.com>
2024-07-08 08:07:13 +0530

    #coding:

--- 5. Machine Learning Sci_Kit_Learn/4 Logistic_Regression_in_Scikit-learn.py
+++ 5. Machine Learning Sci_Kit_Learn/4 Logistic_Regression_in_Scikit-learn.py
@@ -1,4 +1,13 @@
 # 4 Logistic_Regression_in_Scikit-learn
 
 '''
-3  types of Logistic regression '''
+3 Types of Logistic regression:
+1. Binary : Result are 2. 
+Eg:  0 or 1, Yes or No. 
+2. Nominal : 3+ result and no natural ordering.
+Eg: Result for iris classification which has multiple result and there is no natural order.
+3. Ordinal : 3+ result and natural ordering.
+Eg: Result for which has level as good, better, best. 
+'''
+
+ 3+ result and no natural ordering. 3+ result and no natural ordering.
