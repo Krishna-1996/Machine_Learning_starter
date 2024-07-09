62842847e9c2933e806479c5b9793fde77405e83
Krishna-1996 <1904krishna@gmail.com>
2024-07-09 09:15:32 +0530

    # Check accuracy

--- 5. Machine Learning Sci_Kit_Learn/5 K_Nearest_Neighbors.py
+++ 5. Machine Learning Sci_Kit_Learn/5 K_Nearest_Neighbors.py
@@ -12,3 +12,9 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, t_test  = train_test_split(wine.data, wine.target, test_size=0.3)
 
 # Model for K = 7
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=7)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+
+# Check accuracy
