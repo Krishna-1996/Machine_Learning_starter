95dcc3e5475176e365027a0e82adaf8ca9d71296
Krishna-1996 <1904krishna@gmail.com>
2024-07-09 09:13:33 +0530

    # Model for K = 7

--- 5. Machine Learning Sci_Kit_Learn/5 K_Nearest_Neighbors.py
+++ 5. Machine Learning Sci_Kit_Learn/5 K_Nearest_Neighbors.py
@@ -8,3 +8,7 @@ df = pd.DataFrame(data=wine.data, columns=wine.feature_names)
 print(df)
 
 # without pre-processing 
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, t_test  = train_test_split(wine.data, wine.target, test_size=0.3)
+
+# Model for K = 7
