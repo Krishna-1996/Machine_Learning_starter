557de179e66180f457edb2c00cff1dd4e7a0aa62
Krishna-1996 <1904krishna@gmail.com>
2024-07-10 08:25:37 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.6 Decision_Tree_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.6 Decision_Tree_in_Scikit_Learn.py
@@ -10,3 +10,19 @@ print(dataset.head)
 # Adding data 
 x = dataset.drop('Class', axis = 1)# x is whole except the class which is target feature of test result 
 y = dataset['Class']
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+# PreProcessing
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier()
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print("Confusion_metrics: ", confusion_matrix(y_test, y_pred))
+print("classification_report: ", classification_report(y_test, y_pred))
+
+import matplotlib as plt
+from sklearn import metrics
+cm = metrics.confusion_matrixDisplay
