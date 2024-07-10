766118a21c29b488c7f82e517c71d7a3bdc5d9f0
Krishna-1996 <1904krishna@gmail.com>
2024-07-10 08:17:55 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.6 Decision_Tree_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.6 Decision_Tree_in_Scikit_Learn.py
@@ -6,3 +6,7 @@ import numpy as np
 dataset = pd.read_csv("E:/Machine Learning/Machine Learning/bill_authentication.csv")
 print(dataset.shape)
 print(dataset.head)
+
+# Adding data 
+x = dataset.drop('Class', axis = 1)# x is whole except the class which is target feature of test result 
+y = dataset['Class']
