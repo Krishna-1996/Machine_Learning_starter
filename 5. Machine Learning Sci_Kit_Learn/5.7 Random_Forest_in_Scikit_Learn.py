d2f210af2dcbb45051d8f787fd992a023b2dbc92
Krishna-1996 <1904krishna@gmail.com>
2024-07-11 08:51:20 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
@@ -1,5 +1,6 @@
 # 5.7 Random_Forest_in_Scikit_Learn
 
+from logging import critical
 import pandas as pd
 import numpy as np
 
@@ -13,3 +14,8 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0)
 
 # No Pre-Processing
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10, criterion='entropy')
+classifier.fit(x_train, y_train)
+
+
