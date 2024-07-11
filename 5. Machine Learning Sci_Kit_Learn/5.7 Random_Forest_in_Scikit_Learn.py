36c4da37bc28385e796c4f38e591b5276daec995
Krishna-1996 <1904krishna@gmail.com>
2024-07-11 08:46:52 +0530

    # 5.7 Random_Forest_in_Scikit_Learn

--- 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
@@ -9,4 +9,5 @@ print(dataset.head(5))
 x = dataset.drop('target', axis=1)
 y = dataset['target']
 
-from sklearn.model_selection
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0)
