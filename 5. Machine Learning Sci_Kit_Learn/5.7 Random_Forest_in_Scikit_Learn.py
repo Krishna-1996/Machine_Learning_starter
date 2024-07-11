dee2e9fa1ea2755c92bad19b541b21a9e1e6a2a3
Krishna-1996 <1904krishna@gmail.com>
2024-07-11 08:52:57 +0530

    # Prediction

--- 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
@@ -18,4 +18,7 @@ from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=10, criterion='entropy')
 classifier.fit(x_train, y_train)
 
+# Prediction
+y_pred = classifier.predict(x_test)
+
 
