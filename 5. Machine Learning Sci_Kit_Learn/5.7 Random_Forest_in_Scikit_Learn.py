f05777db5c83c91adbc7f8959bb5638e988c7078
Krishna-1996 <1904krishna@gmail.com>
2024-07-11 08:45:39 +0530

    # 5.7 Random_Forest_in_Scikit_Learn

--- 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.7 Random_Forest_in_Scikit_Learn.py
@@ -4,4 +4,8 @@ import pandas as pd
 import numpy as np
 
 dataset = pd.read_csv("E:/Machine Learning/Machine Learning/heart.csv")
+print(dataset.head(5))
+
+x = dataset.drop('target', axis=1)
+y = dataset['target']
 
