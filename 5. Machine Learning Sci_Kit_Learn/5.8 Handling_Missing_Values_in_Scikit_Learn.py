83b62f44b5a2cc4a6edbe3fec01b4ca1db78722b
Krishna-1996 <1904krishna@gmail.com>
2024-07-12 05:56:27 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.8 Handling_Missing_Values_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.8 Handling_Missing_Values_in_Scikit_Learn.py
@@ -3,7 +3,7 @@
 import numpy  as np
 import pandas as pd
 
-x = pd.DataFrame(np.array([5,3,6,3,np.NaN,np.NaN,np.NaN -2 , 4, 6, np.NaN, 2, 7, 9, 4, 6, np.NaN, 5, np.NaN]).reshape(6,3))
+x = pd.DataFrame(np.array([5,3,6,3,np.NaN,np.NaN,np.NaN -2 , 4, 6, np.NaN, 2, 7, 9, np.NaN, 6, np.NaN, 5, np.NaN]).reshape(6,3))
 x.columns = ['f1','f2','f3',]
 print(x)
 
