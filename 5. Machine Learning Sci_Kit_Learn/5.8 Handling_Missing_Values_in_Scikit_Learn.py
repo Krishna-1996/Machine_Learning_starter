9f8c4d19aa518cfc744eeb8c48e14be35d53f532
Krishna-1996 <1904krishna@gmail.com>
2024-07-12 05:53:22 +0530

    missing values

--- 5. Machine Learning Sci_Kit_Learn/5.8 Handling_Missing_Values_in_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.8 Handling_Missing_Values_in_Scikit_Learn.py
@@ -1,6 +1,10 @@
 # 5.8 Handling_Missing_Values_in_Scikit_Learn
 
+import numpy  as np
+import pandas as pd
 
+x = pd.DataFrame(np.array([5,3,6,3,np.NaN,np.NaN,np.NaN -2 , 4, 6, np.NaN, 2, 7, 9, 4, 6 np.NaN, 5]))
+print(x)
 
 
 
