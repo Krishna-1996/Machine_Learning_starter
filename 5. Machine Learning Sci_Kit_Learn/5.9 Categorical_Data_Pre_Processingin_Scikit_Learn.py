8a2c8039d1108d2396bd292f43ddd3ac443ccdfb
Krishna-1996 <1904krishna@gmail.com>
2024-07-12 23:00:10 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.9 Categorical_Data_Pre_Processingin_Scikit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.9 Categorical_Data_Pre_Processingin_Scikit_Learn.py
@@ -2,17 +2,17 @@
 
 import pandas as pd
 import numpy as np
-
+'''
 data = [
     ['male', 'A+', 'high'],
     ['female', 'B+', 'medium'],
-    ['male', 'A-', 'low'],
-    ['female', 'B-', 'high'],
+    ['male', 'A+', 'low'],
+    ['female', 'B+', 'high'],
     ['male', 'O+', 'medium'],
     ['female', 'A+', 'low'],
     ['male', 'B+', 'high'],
-    ['female', 'A-', 'medium'],
-    ['male', 'B-', 'low'],
+    ['female', 'A+', 'medium'],
+    ['male', 'B+', 'low'],
     ['female', 'O+', 'high']
 ]
 
@@ -23,3 +23,16 @@ arr = np.array(data)
 columns = ['Gender', 'Blood_Group', 'Edu_Level']
 df = pd.DataFrame(arr, columns=columns)
 print(df)
+'''
+x = pd.DataFrame(np.array(['male', 'A+', 'high',
+    'female', 'B+', 'medium',
+    'male', 'A+', 'low',
+    'female', 'B+', 'high',
+    'male', 'O+', 'medium',
+    'female', 'A+', 'low',
+    'male', 'B+', 'high',
+    'female', 'A+', 'medium',
+    'male', 'B+', 'low',
+    'female', 'O+', 'high']).reshape(10,3))
+dx = x
+
