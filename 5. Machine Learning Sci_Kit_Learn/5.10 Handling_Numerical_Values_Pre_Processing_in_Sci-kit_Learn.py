aad1758ca9c575260eaa28407844a3545ac63e00
Krishna-1996 <1904krishna@gmail.com>
2024-07-13 12:54:11 +0530

    added

--- 5. Machine Learning Sci_Kit_Learn/5.10 Handling_Numerical_Values_Pre_Processing_in_Sci-kit_Learn.py
+++ 5. Machine Learning Sci_Kit_Learn/5.10 Handling_Numerical_Values_Pre_Processing_in_Sci-kit_Learn.py
@@ -4,3 +4,11 @@ import pandas as pd
 import numpy as np
 from sklearn.datasets import load_iris
 # 5.10.1 Discretization
+
+from sklearn.preprocessing import KBinsDiscretizer
+disc = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
+d = load_iris()
+df = pd.DataFrame(data=data, columns=d.features_name)
+print(df.head())
+
+# 5.10.2 Binarization
