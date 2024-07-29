14bc663513759186438fcf0a7439e6b3804a282e
Krishna-1996 <1904krishna@gmail.com>
2024-07-29 17:05:23 +0530

    added

---
+++ 6. Deep Learning Udemy Course/DUDL_PythonCode/7 ANN/Challenge_regression.py
@@ -0,0 +1,7 @@
+# import libraries
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import matplotlib_inline.backend_inline
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
